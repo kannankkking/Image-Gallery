6938ba8d0d85b4fe62619da210ce8fade78f3f4e
kannankkking <nagappanramu44@gmail.com>
2024-07-09 20:21:11 +0530

    Add files via upload
    image gallery

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+   <div className="section">
+    <div className="content">
+      <h1>THE IMAGE GALLERY</h1>
+    </div>
+   </div>
+   
+  );
+}
+
+export default App;
